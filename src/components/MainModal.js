7d54d0fc08545c0379755ebadd9e2f3859a3d6eb
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-06-24 14:17:05 +0200

    initial commit

---
+++ src/components/MainModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainModal = () => {
+  return (
+    <div className="MainModal">
+      <div className="MainModal__title">
+        Mon titre
+      </div>
+    </div>
+  );
+}
+
+export default MainModal;
