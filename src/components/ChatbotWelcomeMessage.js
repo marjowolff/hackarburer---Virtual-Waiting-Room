80c3e933a991884e61d4e54b866b7f7acdb3ba7b
Claire Kodia <claire.kodia@gmail.com>
2020-06-24 23:59:56 +0200

    Finished styling the chatbotWelcomeMessage

--- src/components/ChatbotWelcomeMessage.js
+++ src/components/ChatbotWelcomeMessage.js
@@ -4,11 +4,13 @@ import './ChatbotWelcomeMessage.css'
 const ChatbotWelcomeMessage = ({ handleDisplayChatbot }) => {
   return (
     <div className='ChatbotWelcomeMessage'>
-      <div className='ChatbotWelcomeMessage__message'>
-        <p>Welcome You, <br /> You will be notified as soon as the doctor joins the video consultation.</p>
-        <button className='ChatbotWelcomeMessage__validationButton' onClick={handleDisplayChatbot}>Ok</button>
+      <div className='ChatbotWelcomeMessage__container'>
+        <div className='ChatbotWelcomeMessage__message'>
+          <p>Welcome You, <br /> You will be notified as soon as the doctor joins the video consultation.</p>
+          <button className='ChatbotWelcomeMessage__validationButton' onClick={handleDisplayChatbot}>Ok</button>
+        </div>
+        <img className='ChatbotWelcomeMessage__avatar' src='https://via.placeholder.com/60' alt='chatbot avatar' />
       </div>
-      <img className='ChatbotWelcomeMessage__avatar' src='https://via.placeholder.com/60' alt='chatbot avatar' />
     </div>
   )
 }
