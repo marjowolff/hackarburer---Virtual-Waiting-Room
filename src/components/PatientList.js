be074a57d940b82e0dddedb58ef75f3d1c0ca7e1
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-06-24 17:20:22 +0200

    patient list

--- src/components/PatientList.js
+++ src/components/PatientList.js
@@ -1,41 +1,54 @@
 import React from 'react';
 import Patient from './Patient'
 import './PatientList.css'
+import FlipMove from 'react-flip-move'; 
 
+class PatientList extends React.Component {
 
-const patients = [
-  {
-    id:1,
-    name: 'Patient A',
-    status:'other'
-  },
-  {
-    id:2,
-    name: 'Patient B',
-    status:'other'
-  },
-  {
-    id:3,
-    name: 'You',
-    status:'you'
-  },
-  {
-    id:4,
-    name: 'Patient D',
-    status:'other'
-  }
-];
-
-
-const PatientList = () => (
-    <div className = "PatientList">
+    state = {
+      patients : [
         {
-          patients.map(patient => 
+          id:1,
+          name: 'Patient A',
+          status:'other'
+        },
+        {
+          id:2,
+          name: 'Patient B',
+          status:'other'
+        },
+        {
+          id:3,
+          name: 'You',
+          status:'you'
+        },
+        {
+          id:4,
+          name: 'Patient D',
+          status:'other'
+        }
+      ]
+    }
+
+    handleClick = () => {
+      console.log('Flipped')
+      this.setState ({})
+    }
+
+    render () {
+      return (<div className = "PatientList">
+      <FlipMove>
+      {
+          this.state.patients.map(patient => 
           <div key={patient.id}><Patient name={patient.name} status={patient.status}/>
           </div>)
-         }     
-      </div>
-)
+         }  
+      </FlipMove>
+      <button onClick={this.handleClick}>Flip Move</button>
+      </div>)
+    }
+    
+}
 
 
 export default PatientList;
