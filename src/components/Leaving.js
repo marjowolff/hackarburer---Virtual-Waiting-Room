8fb2ca6b067e7d7bcb4bbf8bebb921e09ad1d062
Julien Rousseau <julienrousseau16@gmail.com>
2020-06-24 18:55:00 +0200

    leaving modal added and stylized + className modified with "__"

--- src/components/Leaving.js
+++ src/components/Leaving.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 import './Leaving.css'
 
-const Leaving = () => {
+const Leaving = ({leaving, hide}) => {
 
     return (
-        <div>
-            <h1>Leaving ?</h1>
+        <div className='Leaving' style={leaving ?{display: 'block'}:{display: 'none'}}>
+            <div className='Leaving__container'>
+                <p>Before leaving the waiting room, please tell us the reason so that the practitioner may know about it :</p>
+                <textarea></textarea>
+                <div className='Leaving__container__actions'>
+                    <button onClick={hide} id='LCA-leave'>send and leave</button>
+                    <button onClick={hide} id='LCA-stay'>Back to Waiting Room</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/MainModal.js
+++ src/components/MainModal.js
@@ -1,28 +1,38 @@
 import React, { useState } from 'react';
 
-import Counter from './Counter'
+import Counter from './Counter';
+import Leaving from './Leaving';
 import PatientList from './PatientList';
 
 import './MainModal.css'
 
 const MainModal = ({patients}) => {
   const[leaving, setLeaving] = useState(false)
+
+  const showModal = () => {
+    setLeaving(true)
+  }
+  const hideModal = () => {
+    setLeaving(false)
+  }
+  
   return (
     <div className="MainModal">
-      <div className="MainModal_title">
-        <div className='MainModal_title_left'>
+      <div className="MainModal__title">
+        <div className='MainModal__title__left'>
           <img src={require('../images/logo.png')} alt='waiting_room_logo'/>
           <h2>Virtual Waiting Room</h2>
         </div>
-        <div className='MainModal_title_right'>
+        <div className='MainModal__title__right'>
           <p>Need to be somewhere else ?</p>
-          <button>Leave</button>
+          <button onClick={showModal}>Leave</button>
         </div>
       </div>
-      <div className='MainModal_container'>
+      <div className='MainModal__container'>
       <Counter />
       <PatientList patients={patients}/>
       </div>
+      <Leaving leaving={leaving} hide={hideModal}/>
     </div>
   );
 }
