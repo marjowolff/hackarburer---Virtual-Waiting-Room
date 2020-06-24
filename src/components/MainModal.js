09ed487f260cf568c246964ebd81ff79adb9b21f
Claire Kodia <claire.kodia@gmail.com>
2020-06-24 22:44:47 +0200

    Imported the chatbotwelcomemessagecomponent and set up the conditional display of the components

--- src/components/MainModal.js
+++ src/components/MainModal.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import Chatbot from './Chatbot';
+import React, { useState } from 'react'
+import Chatbot from './Chatbot'
 import Counter from './Counter'
-import Office from './Office';
-import PatientList from './PatientList';
+import Office from './Office'
+import PatientList from './PatientList'
 
 import './MainModal.css'
+import ChatbotWelcomeMessage from './ChatbotWelcomeMessage'
 
-const MainModal = ({patients, timeAppointement}) => {
+const MainModal = ({ patients, timeAppointement }) => {
+  const [displayChatbot, setDisplayChatbot] = useState(false)
+  
+  const handleDisplayChatbot = () => setDisplayChatbot(true)
+
   return (
-    <div className="MainModal">
-      <div className="MainModal_title">
+    <div className='MainModal'>
+      <div className='MainModal_title'>
         <div className='MainModal_title_left'>
-          <img src={require('../images/logo.png')} alt='waiting_room_logo'/>
+          <img src={require('../images/logo.png')} alt='waiting_room_logo' />
           <h2>Virtual Waiting Room</h2>
         </div>
         <div className='MainModal_title_right'>
@@ -21,15 +26,15 @@ const MainModal = ({patients, timeAppointement}) => {
       <div className='MainModal_container'>
         <div className='MainModal_container_left'>
           <Office timeAppointement={timeAppointement} patient={patients[0]} />
-          <PatientList patients={patients}/>
+          <PatientList patients={patients} />
         </div>
         <div className='MainModal_container_right'>
-          <Counter patients={patients}/>
-          <Chatbot />
+          <Counter patients={patients} />
+          {displayChatbot ? <Chatbot /> : <ChatbotWelcomeMessage handleDisplayChatbot={handleDisplayChatbot} />}
         </div>
       </div>
     </div>
   )
 }
 
-export default MainModal;
+export default MainModal
