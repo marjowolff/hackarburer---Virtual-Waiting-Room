27b92e7aa9c37f7204167079061b83ae090ef9de
Julien Rousseau <julienrousseau16@gmail.com>
2020-06-24 15:46:21 +0200

    modal title background-color changed + LEAVE button added

--- src/components/MainModal.js
+++ src/components/MainModal.js
@@ -6,8 +6,13 @@ const MainModal = () => {
   return (
     <div className="MainModal">
       <div className="MainModal_title">
-        <img src={require('../images/logo.png')} alt='waiting_room_logo'/>
-        <h2>Virtual Waiting Room</h2>
+        <div className='MainModal_title_left'>
+          <img src={require('../images/logo.png')} alt='waiting_room_logo'/>
+          <h2>Virtual Waiting Room</h2>
+        </div>
+        <div className='MainModal_title_right'>
+          <button>Leave</button>
+        </div>
       </div>
       <div className='MainModal_container'>
       </div>
