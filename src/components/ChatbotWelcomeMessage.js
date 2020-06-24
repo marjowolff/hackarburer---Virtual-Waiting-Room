5c254702b6f1e57ff5fec8a85906bb193aba66bc
Claire Kodia <claire.kodia@gmail.com>
2020-06-24 23:07:10 +0200

    Finalized the structure of the message

--- src/components/ChatbotWelcomeMessage.js
+++ src/components/ChatbotWelcomeMessage.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import './ChatbotWelcomeMessage.css'
 
 const ChatbotWelcomeMessage = ({ handleDisplayChatbot }) => {
   return (
     <div>
-      hello
-      <button onClick={handleDisplayChatbot}>Ok</button>
+      <div>
+        <p>Welcome You, <br /> You will be notified as soon as the doctor joins the video consultation.</p>
+        <button onClick={handleDisplayChatbot}>Ok</button>
+      </div>
+      <img className='Chatbot__avatar' src='https://via.placeholder.com/60' alt='chatbot avatar' />
     </div>
   )
 }
