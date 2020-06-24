e9d1128b15eb6c4927168ba0b3819b1e0b7fd207
Julien Rousseau <julienrousseau16@gmail.com>
2020-06-24 15:04:28 +0200

    MainModal component created and stylized

--- src/components/MainModal.js
+++ src/components/MainModal.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import './MainModal.css'
+
 const MainModal = () => {
   return (
     <div className="MainModal">
-      <div className="MainModal__title">
-        Mon titre
+      <div className="MainModal_title">
+        <img src={require('../images/logo.png')} alt='waiting_room_logo'/>
+        <h2>Virtual Waiting Room</h2>
+      </div>
+      <div className='MainModal_container'>
       </div>
     </div>
   );
