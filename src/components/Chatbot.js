64ed1d499314d3ee5cf6dec87a61646549e94c42
LenaSim <radical.rin@gmail.com>
2020-06-24 20:17:28 +0200

    Change color and font of chatbox

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -149,11 +149,11 @@ const steps = [
 
 const theme = {
   background: '#f5f8fb',
-  fontFamily: 'Helvetica Neue',
-  headerBgColor: '#EF6C00',
+  fontFamily: 'Roboto',
+  headerBgColor: '#FF7E00',
   headerFontColor: '#fff',
   headerFontSize: '15px',
-  botBubbleColor: '#EF6C00',
+  botBubbleColor: '#FF7E00',
   botFontColor: '#fff',
   userBubbleColor: '#fff',
   userFontColor: '#4a4a4a',
