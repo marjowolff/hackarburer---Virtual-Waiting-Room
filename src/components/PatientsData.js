facd56b9da5dc19ed73fdfc5237241db319f8bf9
LenaSim <radical.rin@gmail.com>
2020-06-25 10:24:14 +0200

    Divide virtual waiting room and patients data file

---
+++ src/components/PatientsData.js
@@ -0,0 +1,44 @@
+const initialPatientsData = [
+  {
+    id:1,
+    name: 'Patient A',
+    status:'other'
+  },
+  {
+    id:2,
+    name: 'Patient B',
+    status:'other'
+  },
+  {
+    id:3,
+    name: 'Patient C',
+    status:'other'
+  },
+  {
+    id:4,
+    name: 'You',
+    status:'you'
+  },
+  {
+    id:5,
+    name: 'Patient E',
+    status:'other'
+  },
+  {
+    id:6,
+    name: 'Patient F',
+    status:'other'
+  },
+  {
+    id:7,
+    name: 'Patient G',
+    status:'other'
+  },
+  {
+    id:8,
+    name: 'Patient H',
+    status:'other'
+  }
+]
+
+export default initialPatientsData
---
+++ src/components/VirtualWaitingRoom.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+
+import MainModal from './MainModal'
+// import Navbar from './Navbar'
+import DocInterface from './DocInterface'
+import initialPatientsData from './PatientsData'
+
+
+
+const VirtualWaitingRoom = () => {
+  const [patients, setPatients] = useState(initialPatientsData)
+  const [timeAppointement, setTimeAppointement] = useState(0)
+  const [anim,setAnim] = useState(false)
+  const [docInterface, setDocInterface] = useState(false)
+  const [timeAnim, setTimeAnim] = useState(false)
+
+  const handleClick = () => {
+    //remove patient #1
+    const tempPatients = [...patients].slice(1)
+    setPatients(tempPatients)
+    //setTimeAppointement to 0
+    setAnim(true)
+    setTimeout(()=> setAnim(false), 3000)
+  }
+
+  // useEffect(() => {setInterval(() => {setTimeAppointement(timeAppointement + 1)}, 3000)}, [timeAppointement])
+  //   setTimeAppointement(0)
+  
+
+  const showDocInterface = () => {
+    setDocInterface(true)
+  }
+
+  useEffect(() => {
+    let interval = null;
+    interval = setInterval(() => {
+      setTimeAppointement(timeAppointement => timeAppointement + 1)
+      setTimeAnim(true)
+      setTimeout(()=> setTimeAnim(false), 2000)
+      }, 3000);
+    return () => clearInterval(interval);
+  }, [timeAppointement]);
+
+  return (
+    <div >
+      {/* <Navbar /> */}
+      <div className='App__container'>
+        <MainModal patients={patients} timeAppointement={timeAppointement} timeAnim={timeAnim} showDoc={showDocInterface} anim={anim}/>
+        <DocInterface patients={patients} newPatient={handleClick} docInterface={docInterface} />
+      </div>
+    </div>
+  );
+}
+
+export default VirtualWaitingRoom;
