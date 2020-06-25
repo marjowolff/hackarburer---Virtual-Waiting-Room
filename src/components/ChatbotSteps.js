53b9525a5931c2cf143feb02dd3edfc8bf450c50
Claire Kodia <claire.kodia@gmail.com>
2020-06-25 11:11:26 +0200

    managed to change the heigth and start positionnng the bot

---
+++ src/components/ChatbotSteps.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import UserInfoView from './UserInfoView'
+
+const patientName = 'Titi'
+const doctorName = 'Dr.Stranger'
+
+export const steps = [
+  {
+    id: '0',
+    message: `Hello ${patientName}, I'm here to help you ! I'm so happy to have you here for your first visit to ${doctorName}.`,
+    delay: 2000,
+    trigger: '1'
+  },
+  {
+    id: '1',
+    message: 'While you wait, why not take some time to provide us with some information ?',
+    trigger: 'followup'
+  },
+  {
+    id: 'followup',
+    message: 'Choose any category',
+    trigger: '2'
+  },
+  {
+    id: '2',
+    delay: 3000,
+    options: [
+      { value: 1, label: '1 - Personal information', trigger: '4' },
+      { value: 2, label: '2 - Preliminary questions', trigger: '5' }
+      // { value: 3, label: '3 Read', trigger: '8' },
+    ]
+  },
+  {
+    id: '3',
+    message: 'Your information is successfully updated',
+    trigger: 'review'
+  },
+  {
+    id: '4',
+    message: 'Confirm the following information',
+    trigger: 'review'
+  },
+  {
+    id: 'review',
+    component: <UserInfoView />,
+    asMessage: true,
+    trigger: 'update'
+  },
+  {
+    id: 'update',
+    message: 'Would you like to update some field?',
+    trigger: 'update-question'
+  },
+  {
+    id: 'update-question',
+    options: [
+      { value: 'yes', label: 'Yes', trigger: 'update-yes' },
+      { value: 'no', label: 'No', trigger: 'personal-info-end-message' }
+    ]
+  },
+  {
+    id: 'update-yes',
+    message: 'What field would you like to update?',
+    trigger: 'update-fields'
+  },
+  {
+    id: 'update-fields',
+    options: [
+      { value: 'number', label: 'Phone Number', trigger: 'update-number' },
+      { value: 'address', label: 'Home address', trigger: 'update-address' },
+      { value: 'birthday', label: 'Birthday', trigger: 'update-birthday' }
+    ]
+  },
+  {
+    id: 'update-number',
+    message: 'What is your updated phone number?',
+    trigger: 'number'
+  },
+  {
+    id: 'number',
+    user: true,
+    trigger: '4'
+  },
+  {
+    id: 'update-address',
+    message: 'What is your updated address?',
+    trigger: 'address'
+  },
+  {
+    id: 'address',
+    user: true,
+    trigger: '4'
+  },
+  {
+    id: 'update-birthday',
+    message: 'What is your updated birthday?',
+    trigger: 'birthday'
+  },
+  {
+    id: 'birthday',
+    user: true,
+    trigger: '4'
+  },
+  {
+    id: 'personal-info-end-message',
+    message: 'Thanks! Your data has been submitted successfully!',
+    trigger: '10'
+  },
+  {
+    id: '5',
+    message: 'Do you have any allergies?',
+    delay: 3500,
+    trigger: 'allergy'
+  },
+  {
+    id: 'allergy',
+    options: [
+      { value: 'yes', label: 'Yes', trigger: '6' },
+      { value: 'no', label: 'No', trigger: '6' }
+    ]
+  },
+  {
+    id: '6',
+    message: 'Do you smoke?',
+    trigger: 'smoke'
+  },
+  {
+    id: 'smoke',
+    options: [
+      { value: 'yes', label: 'Yes', trigger: '7' },
+      { value: 'no', label: 'No', trigger: '7' }
+    ]
+  },
+  {
+    id: '7',
+    message: 'Are you taking any medication?',
+    trigger: 'medication'
+  },
+  {
+    id: 'medication',
+    options: [
+      { value: 'yes', label: 'Yes', trigger: '8' },
+      { value: 'no', label: 'No', trigger: '8' }
+    ]
+  },
+  {
+    id: '8',
+    message: 'Thank you ! All your answers would be considered carefully by the doctor',
+    trigger: '9'
+  },
+  {
+    id: '10',
+    message: 'Still here? Now, let\'s answer some preliminary questions',
+    delay: 4000,
+    trigger: '5'
+  },
+  {
+    id: '9',
+    message: 'Bye!',
+    end: true
+  }
+]
--- src/components/MainModal.js
+++ src/components/MainModal.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
-import Chatbot from './Chatbot';
+import React, { useState } from 'react'
+import Chatbot from './Chatbot'
 import ChatbotWelcomeMessage from './ChatbotWelcomeMessage'
 import Counter from './Counter'
-import Leaving from './Leaving';
-import Office from './Office';
-import PatientList from './PatientList';
+import Leaving from './Leaving'
+import Office from './Office'
+import PatientList from './PatientList'
 
 import './MainModal.css'
 
-const MainModal = ({ patients, timeAppointement, timeAnim, showDoc,anim }) => {
+const MainModal = ({ patients, timeAppointement, timeAnim, showDoc, anim }) => {
   const [displayChatbot, setDisplayChatbot] = useState(false)
-  const[leaving, setLeaving] = useState(false)
-  
+  const [leaving, setLeaving] = useState(false)
+
   const handleDisplayChatbot = () => setDisplayChatbot(true)
-  
-  const showModal = () => {
-    setLeaving(true)
-  }
-  const hideModal = () => {
-    setLeaving(false)
-  }
+  const showModal = () => setLeaving(true)
+  const hideModal = () => setLeaving(false)
+
+  const botAvatarUrl = 'https://via.placeholder.com/60' // Cf exmeple d'integration dans le chatbot: https://github.com/btotharye/cisco-pss-api-chatbot/blob/master/src/App.js
+  const userAvatarUrl = 'https://via.placeholder.com/60'
+
   return (
-    <div className="MainModal">
-      <div className="MainModal__title">
+    <div className='MainModal'>
+      <div className='MainModal__title'>
         <div className='MainModal__title__left'>
-          <img src={require('../images/logo.png')} alt='waiting_room_logo'/>
+          <img src={require('../images/logo.png')} alt='waiting_room_logo' />
           <h2 onClick={showDoc}>Virtual Waiting Room</h2>
         </div>
         <div className='MainModal__title__right'>
@@ -35,15 +34,15 @@ const MainModal = ({ patients, timeAppointement, timeAnim, showDoc,anim }) => {
 
       <div className='MainModal__container'>
         <div className='MainModal__container__left'>
-          <Office timeAppointement={timeAppointement} patient={patients[0]} anim={anim}/>
+          <Office timeAppointement={timeAppointement} patient={patients[0]} anim={anim} />
           <PatientList patients={patients} />
         </div>
         <div className='MainModal_container_right'>
           <Counter patients={patients} timeAppointement={timeAppointement} timeAnim={timeAnim} />
-          {displayChatbot ? <Chatbot /> : <ChatbotWelcomeMessage handleDisplayChatbot={handleDisplayChatbot} />}
+          {displayChatbot ? <Chatbot botAvatarUrl={botAvatarUrl} userAvatarUrl={userAvatarUrl} /> : <ChatbotWelcomeMessage handleDisplayChatbot={handleDisplayChatbot} botAvatarUrl={botAvatarUrl} />}
         </div>
       </div>
-      <Leaving leaving={leaving} hide={hideModal}/>
+      <Leaving leaving={leaving} hide={hideModal} />
     </div>
   )
 }
