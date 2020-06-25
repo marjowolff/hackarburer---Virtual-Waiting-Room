a085ec69a657bc370b9646b61286b7900bcb0704
Claire Kodia <claire.kodia@gmail.com>
2020-06-25 16:33:49 +0200

    Finished for the video

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import FrontPage from './components/FrontPage'
 import VirtualWaitingRoom from './components/VirtualWaitingRoom'
 
-import './App.css';
+import './App.css'
 
 const App = () => {
-
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
-        <Switch>
-          <Route exact path="/" component={FrontPage} />
-          <Route path="/waitingroom" component={VirtualWaitingRoom} />
-        </Switch>
+      <Switch>
+        <Route exact path='/' component={FrontPage} />
+        <Route path='/waitingroom' component={VirtualWaitingRoom} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Patient.js
+++ src/components/Patient.js
@@ -1,30 +1,35 @@
-import React, {useState} from 'react';
+import React from 'react'
+import ReactPlayer from 'react-player'
 import './Patient.css'
-import ReactPlayer from 'react-player/youtube'
-import myVideo from '../images/webcam.mov'
-
-
 
 const Patient = (props) => {
   return (
-    <div className="Patient">
-      {props.status === 'you' ? 
-        (<div className = 'Patient__PlayerWrapper' >
-          <ReactPlayer className='react-player' url='https://youtu.be/ObIdE94YQJU?showinfo=0' playing = {true} loop={true} width='100%' height='100%' config={{
-    youtube: {
-      playerVars: { modestbranding: 1,
-      showinfo : 0,
-      loop:1
-     }
-    }
-  }}/>
-        </div>) 
-        : <img className = 'Patient__Img' src={require(`../${props.pic}`)} />}
-      {props.status === 'you' ?
-      <div className="Patient__Name Patient__Name__You"> {props.name}</div> : <div className="Patient__Name"> {props.name}</div>}
-    </div> 
-    
-  );
+    <div className='Patient'>
+      {props.status === 'you'
+        ? (<div className='Patient__PlayerWrapper'>
+          <ReactPlayer
+            url='https://youtu.be/ObIdE94YQJU?showinfo=0'
+            playing
+            loop
+            width='100%'
+            height='100%'
+            style={{ 'pointer-events': 'none' }}
+            config={{
+              youtube: {
+                playerVars: {
+                  modestbranding: 1,
+                  showinfo: 0
+                }
+              }
+            }}
+          />
+        </div>)
+        : <img className='Patient__Img' src={require(`../${props.pic}`)} alt='patient' />}
+      {props.status === 'you'
+        ? <div className='Patient__Name Patient__Name__You'> {props.name}</div> : <div className='Patient__Name'> {props.name}</div>}
+    </div>
+
+  )
 }
-//
-export default Patient;
+
+export default Patient
